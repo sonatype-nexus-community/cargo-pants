2ab9cbc9e9323df8384f3ee5ab4e68edc0f954f6
Glenn Mohre <glenn.mohre@gmail.com>
2019-03-22 13:40:22 -0400

    added test to coordinates

--- src/coordinate.rs
+++ src/coordinate.rs
@@ -36,3 +36,22 @@ impl fmt::Display for Vulnerability {
         write!(f, "{}\n{}\n{}\n{}\n{}", self.title, self.description, self.cvss_score, self.cvss_vector, self.reference)
     }
 }
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use mockito;
+
+    #[test]
+    fn test_parse_bytes() {
+        let raw_json: &[u8] = r##"{
+            "coordinates": "pkg:pypi/rust@0.1.1",
+            "description": "Ribo-Seq Unit Step Transformation",
+            "reference": "https://ossindex.sonatype.org/component/pkg:pypi/rust@0.1.1",
+            "vulnerabilities": []
+        }"##.as_bytes();
+        let coordinate: Coordinate =
+            serde_json::from_slice(raw_json).unwrap();
+        assert_eq!(coordinate.reference, "https://ossindex.sonatype.org/component/pkg:pypi/rust@0.1.1");
+        assert_eq!(coordinate.description, "Ribo-Seq Unit Step Transformation");
+    }
+}
