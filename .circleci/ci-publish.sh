66ef7498a38e95df31eaaf3f216a0fa984c0e44e
Jeffry Hesse <5544326+DarthHater@users.noreply.github.com>
2020-06-30 19:28:14 -0800

    Cargo release

--- .circleci/ci-publish.sh
+++ .circleci/ci-publish.sh
@@ -1,16 +1,8 @@
 #!/usr/bin/env bash
-cargo install cargo-bump --force
+cargo install cargo-release --force
 
 git config user.name "$GITHUB_USERNAME"
 
 git config user.email "$GITHUB_EMAIL"
 
-cargo bump $CIRCLE_TAG
-
-git add .
-
-git commit -m "[skip ci] release of $CIRCLE_TAG"
-
-git push origin master
-
-cargo publish
+cargo release 
