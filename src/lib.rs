7013e2a5255887a2e7529e7a7dab0035ff9528fb
Michael Kearns <mobiuscog@users.noreply.github.com>
2021-03-08 19:20:53 +0000

    Fix formatting in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -24,18 +24,18 @@ extern crate serde_json;
 
 pub mod client;
 pub mod coordinate;
-pub mod vulnerability;
 pub mod error;
 pub mod lockfile;
 pub mod package;
+pub mod vulnerability;
 
-pub use crate::{client::*, coordinate::*, vulnerability::*, error::*, lockfile::*, package::*};
+pub use crate::{client::*, coordinate::*, error::*, lockfile::*, package::*, vulnerability::*};
 
 // Global Singletons are bad kids. Don't use them (unless you need the terminal width everywhere).
-const DEFAULT_TERM_SIZE:termsize::Size = termsize::Size{cols: 80, rows: 40};
+const DEFAULT_TERM_SIZE: termsize::Size = termsize::Size { cols: 80, rows: 40 };
 use lazy_static::lazy_static;
 lazy_static! {
-    pub static ref TERM_WIDTH:u16 = calculate_term_width();
+    pub static ref TERM_WIDTH: u16 = calculate_term_width();
 }
 
 fn calculate_term_width() -> u16 {
