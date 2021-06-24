7b897c0041938e52d82289d0567c55e701398212
Amelia Keibler <akeibler@sonatype.com>
2021-06-24 09:17:32 -0400

    Refactor Error to follow thiserror's conventions
    Move from failure's ErrorKind pattern to thiserror's macro driven from
    implementations. This also avoids collapsing errors to strings and losing
    context early

--- src/error.rs
+++ src/error.rs
@@ -15,111 +15,28 @@
 
 #[cfg(feature = "chrono")]
 use chrono;
-use std::{
-    fmt::{self, Display},
-    io,
-    str::Utf8Error,
-};
+use std::str::Utf8Error;
 use toml;
 
-/// Error type
-#[derive(Debug)]
-pub struct Error {
-    /// Contextual information about the error
-    inner: ErrorKind,
-
-    /// Description of the error providing additional information
-    description: String,
-}
-
-impl Error {
-    /// Create a new error with the given description
-    pub fn new<S: ToString>(kind: ErrorKind, description: &S) -> Self {
-        Self {
-            inner: kind,
-            description: description.to_string(),
-        }
-    }
-
-    /// Obtain the inner `ErrorKind` for this error
-    pub fn kind(&self) -> ErrorKind {
-        self.inner
-    }
-}
-
-impl Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}: {}", &self.inner, &self.description)
-    }
-}
-
-/// Custom error type for this library
-#[derive(Copy, Clone, Debug, Eq, thiserror::Error, PartialEq)]
-pub enum ErrorKind {
-    /// Invalid argument or parameter
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
     #[error("bad parameter")]
     BadParam,
 
     /// An error occurred performing an I/O operation (e.g. network, file)
     #[error("I/O operation failed")]
-    Io,
+    Io(#[from] std::io::Error),
 
     /// Couldn't parse response data
+    #[error("couldn't parse data as UTF-8")]
+    ParseUtf8(Utf8Error),
+
+    #[cfg(feature = "chrono")]
+    /// Couldn't parse response data
     #[error("couldn't parse data")]
-    Parse,
-}
+    ParseChrono(#[from] chrono::ParseError),
 
-/// Create a new error (of a given enum variant) with a formatted message
-macro_rules! err {
-    ($kind:path, $msg:expr) => {
-        crate::error::Error::new(
-            $kind,
-            &$msg.to_string()
-        )
-    };
-    ($kind:path, $fmt:expr, $($arg:tt)+) => {
-        err!($kind, &format!($fmt, $($arg)+))
-    };
-}
-
-/// Create and return an error with a formatted message
-#[allow(unused_macros)]
-macro_rules! fail {
-    ($kind:path, $msg:expr) => {
-        return Err(err!($kind, $msg).into());
-    };
-    ($kind:path, $fmt:expr, $($arg:tt)+) => {
-        fail!($kind, &format!($fmt, $($arg)+));
-    };
-}
-
-impl From<Utf8Error> for Error {
-    fn from(other: Utf8Error) -> Self {
-        err!(ErrorKind::Parse, &other)
-    }
-}
-
-#[cfg(feature = "chrono")]
-impl From<chrono::ParseError> for Error {
-    fn from(other: chrono::ParseError) -> Self {
-        err!(ErrorKind::Parse, &other)
-    }
-}
-
-impl From<fmt::Error> for Error {
-    fn from(other: fmt::Error) -> Self {
-        err!(ErrorKind::Io, &other)
-    }
-}
-
-impl From<io::Error> for Error {
-    fn from(other: io::Error) -> Self {
-        err!(ErrorKind::Io, &other)
-    }
-}
-
-impl From<toml::de::Error> for Error {
-    fn from(other: toml::de::Error) -> Self {
-        err!(ErrorKind::Parse, &other)
-    }
+    /// Error processing the Cargo.toml file
+    #[error("couldn't parse the Cargo.toml file")]
+    ParseCargoToml(#[from] toml::de::Error)
 }
