546bb80c1549a58b39a1a8f64be5c9ffa3894216
Jeffry Hesse <5544326+DarthHater@users.noreply.github.com>
2021-08-19 14:32:40 -0800

    Move to buf reader

--- src/lib.rs
+++ src/lib.rs
@@ -21,9 +21,10 @@ extern crate serde_derive;
 extern crate log;
 extern crate serde_json;
 
+use std::io::BufReader;
+use std::fs::File;
 use std::path::PathBuf;
 use terminal_size::{terminal_size, Height, Width};
-use std::fs;
 
 pub mod client;
 pub mod common;
@@ -65,8 +66,9 @@ pub struct Ignore {
 }
 
 pub fn filter_vulnerabilities(packages: &mut Vec<Coordinate>, exclude_vuln_file_path: PathBuf) {
-    let filter_list_str = fs::read_to_string(exclude_vuln_file_path).expect("Unable to read file");
-    let filter_list_json: FilterList = serde_json::from_str(&filter_list_str).expect("JSON was not well formatted");
+    let file = File::open(exclude_vuln_file_path).expect("Unable to open file");
+    let reader = BufReader::new(file);
+    let filter_list_json: FilterList = serde_json::from_reader(reader).expect("JSON was not well formatted");
 
     let ids = filter_list_json.ignore.into_iter().map(|filter| filter.id ).collect::<Vec<String>>();
 
